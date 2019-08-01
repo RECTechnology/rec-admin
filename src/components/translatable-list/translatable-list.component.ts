0751d27fce8409482241d5efaf54f0b687f56bf3
nombrekeff <manoloedge96@gmail.com>
2019-08-01 13:13:43 +0200

    b2b: added sorting to b2b settings lists

--- src/components/translatable-list/translatable-list.component.ts
+++ src/components/translatable-list/translatable-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'translatable-list',
@@ -14,10 +14,12 @@ export class TranslatableListComponent implements OnInit {
   @Output('delete') public onDelete: EventEmitter<any> = new EventEmitter();
 
   @ViewChild(MatPaginator, { static: true }) public paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) public sort: MatSort;
 
   public ngOnInit() {
     this.dataSource = new MatTableDataSource<any>(this.data);
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 }
