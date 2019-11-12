23634e498d4b51ce0d488e5edbe2e2fed75b65b3
nombrekeff <manoloedge96@gmail.com>
2019-11-12 12:17:06 +0100

    account->lemonway: started adding info

--- src/pages/account/tab_lemonway/lemonway.tab.ts
+++ src/pages/account/tab_lemonway/lemonway.tab.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { B2bService } from 'src/services/b2b/b2b.service';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
 import { TlHeader, TlItemOption, TableListOptions } from 'src/components/table-list/tl-table/tl-table.component';
 import { TablePageBase } from 'src/bases/page-base';
 import { ControlesService } from 'src/services/controles/controles.service';
-import { Router } from '@angular/router';
 import { LoginService } from 'src/services/auth/auth.service';
-import { Title } from '@angular/platform-browser';
 import { AlertsService } from 'src/services/alerts/alerts.service';
 import { CreateLemonWithdrawalDia } from 'src/dialogs/create-lemon-withdrawal/create-lemon-withdrawal.dia';
 
@@ -23,6 +23,14 @@ export class LemonWayTab extends TablePageBase {
     {
       sort: 'id',
       title: 'ID',
+      type: 'code',
+    }, {
+      sort: 'concept',
+      title: 'Concept',
+      type: 'text',
+    }, {
+      sort: 'amount',
+      title: 'Amount',
       type: 'code',
     },
   ];
@@ -44,13 +52,13 @@ export class LemonWayTab extends TablePageBase {
       },
     },
   ];
+
   public options: TableListOptions = {
     optionsType: 'buttons',
   };
 
   constructor(
     public controles: ControlesService,
-    public b2bCrud: B2bService,
     public router: Router,
     public ls: LoginService,
     public titleService: Title,
