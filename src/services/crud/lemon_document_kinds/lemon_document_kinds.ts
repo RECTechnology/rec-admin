72df8c144086a20c60563d4d7c38976acc4a6aa7
nombrekeff <manoloedge96@gmail.com>
2019-12-11 12:35:05 +0100

    feat: preparing Create document, to allow to add lemonway document kinds

---
+++ src/services/crud/lemon_document_kinds/lemon_document_kinds.ts
@@ -0,0 +1,42 @@
+
+import { Injectable } from '@angular/core';
+import { CrudBaseService } from 'src/services/base/crud.base';
+import { UserService } from 'src/services/user.service';
+import { HttpClient } from '@angular/common/http';
+import { CompanyService } from 'src/services/company/company.service';
+import { DocumentKind } from 'src/shared/entities/document_kind.ent';
+
+@Injectable()
+export class LemonDocumentKindsCrud extends CrudBaseService<DocumentKind> {
+
+    public pdfHtml: string = '';
+
+    constructor(
+        http: HttpClient,
+        public us: UserService,
+        public cs: CompanyService,
+    ) {
+        super(http, us);
+        this.basePath = '/lemon_document_kinds';
+        this.userRole = 'admin';
+        this.mapItems = true;
+    }
+
+    public mapper(item) {
+        return new DocumentKind(item);
+    }
+
+    public setTier(id, tier_id) {
+        return this.update(id, { tier_id });
+    }
+
+    // public unsetTier(id) {
+    //     return this.update(id, { tier_id: 'null' });
+    // }
+
+    // public unsetTier(dock_id, tier_id) {
+    //     const url = [...this.getUrlBase(), '/', dock_id, '/', 'tier', '/', tier_id];
+    //     return this.delete(url);
+    // }
+
+}
