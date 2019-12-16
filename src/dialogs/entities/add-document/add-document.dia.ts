31e36c5ebddd783bf125b206dfcd0401395c774f
nombrekeff <manoloedge96@gmail.com>
2019-12-16 12:58:04 +0100

    fix: fixed lemonway add document, needs to upload lemon doc if kind is lemon

--- src/dialogs/entities/add-document/add-document.dia.ts
+++ src/dialogs/entities/add-document/add-document.dia.ts
@@ -8,6 +8,7 @@ import { DocumentCrud } from 'src/services/crud/documents/documents';
 import { DocumentKindsCrud } from 'src/services/crud/document_kinds/document_kinds';
 import { UtilsService } from 'src/services/utils/utils.service';
 import { FileUpload } from 'src/dialogs/other/file-upload/file-upload.dia';
+import { LemonwayDocumentCrud } from 'src/services/crud/lemonway_documents/lemonway_documents';
 
 @Component({
   selector: 'add-document',
@@ -37,6 +38,7 @@ export class AddDocumentDia extends BaseDialog {
     private us: UserService,
     public alerts: AlertsService,
     public docCrud: DocumentCrud,
+    public lemonDocCrud: LemonwayDocumentCrud,
     public dkCrud: DocumentKindsCrud,
   ) {
     super();
@@ -48,11 +50,15 @@ export class AddDocumentDia extends BaseDialog {
       .subscribe((resp) => {
         this.docKinds = resp.data.elements.map((el) => {
           return {
-            value: el.id,
+            value: el,
             name: el.name,
           };
         });
       });
+  }
+
+  public getCrud(kind) {
+    return (kind.lemon_doctype === undefined) ? this.docCrud : this.lemonDocCrud;
   }
 
   public ngOnInit() {
@@ -69,18 +75,26 @@ export class AddDocumentDia extends BaseDialog {
 
     this.loading = true;
 
-    let data: any = this.item;
+    let data: any = { ...this.item };
     if (this.isEdit) {
-      data = UtilsService.deepDiff(this.item, this.itemCopy);
+      data = UtilsService.deepDiff(data, this.itemCopy);
     }
 
     if (!Object.keys(data).length) {
       return this.alerts.showSnackbar('Nothing to update...');
     }
 
+    if (data.kind) {
+      data.kind_id = data.kind.id;
+    }
+
+    console.log('kinfd', data.kind);
+    const crud = this.getCrud(data.kind);
+    delete data.kind;
+
     const call = (!this.isEdit)
-      ? this.docCrud.create(data)
-      : this.docCrud.update(this.item.id, UtilsService.sanitizeEntityForEdit(data));
+      ? crud.create(data)
+      : crud.update(this.item.id, UtilsService.sanitizeEntityForEdit(data));
 
     call.subscribe((resp) => {
       this.alerts.showSnackbar((this.isEdit ? 'Edited' : 'Created') + ' Document correctly!', 'ok');
@@ -88,7 +102,7 @@ export class AddDocumentDia extends BaseDialog {
       this.close();
     }, (err) => {
       if (err.errors) {
-        this.validationErrors = UtilsService.normalizeLwError(err.errors);
+        this.validationErrors = err.errors; //UtilsService.normalizeLwError(err.errors);
       } else {
         this.alerts.showSnackbar(err.message);
       }
