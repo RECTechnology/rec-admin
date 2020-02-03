58ee53d1a30cb4f8a9eca7861b1a27e2f8c89f5f
nombrekeff <manoloedge96@gmail.com>
2020-02-03 15:54:02 +0100

    .

--- src/pages/special-actions/b2b/entities/components/documents/documents.tab.ts
+++ src/pages/special-actions/b2b/entities/components/documents/documents.tab.ts
@@ -37,10 +37,10 @@ export class DocumentTabComponent extends EntityTabBase<Document> {
         TlHeaders.Description,
     ];
     public itemOptions: TlItemOption[] = [
-        TlItemOptions.Delete(this.deleteItem.bind(this),
-            {
-                ngIf: (item) => item.lemon_reference,
-            }),
+        TlItemOptions.Delete(
+            this.deleteItem.bind(this),
+            { ngIf: (item) => item.lemon_reference },
+        ),
     ];
     public tableOptions: TableListOptions = {
         optionsType: 'buttons',
