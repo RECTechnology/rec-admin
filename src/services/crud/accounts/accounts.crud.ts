c638c47dfd19c10351e0b7b29e4939fbe7057a94
nombrekeff <manoloedge96@gmail.com>
2020-10-08 14:14:02 +0200

    feat: added campaing relationship calls to AccountsCrud Service

--- src/services/crud/accounts/accounts.crud.ts
+++ src/services/crud/accounts/accounts.crud.ts
@@ -12,145 +12,161 @@ import { Iban } from 'src/shared/entities/iban.ent';
 
 @Injectable()
 export class AccountsCrud extends CrudBaseService<Account> {
+  public pdfHtml: string = '';
+  @Inject(EventsService) public events: EventsService;
 
-    public pdfHtml: string = '';
-    @Inject(EventsService) public events: EventsService;
+  constructor(http: HttpClient, public us: UserService, public cs: CompanyService) {
+    super(http, us);
+    this.basePath = '/accounts';
+    this.userRole = 'admin';
+    this.mapItems = true;
+  }
 
-    constructor(
-        http: HttpClient,
-        public us: UserService,
-        public cs: CompanyService,
-    ) {
-        super(http, us);
-        this.basePath = '/accounts';
-        this.userRole = 'admin';
-        this.mapItems = true;
-    }
+  public mapper(item) {
+    return new Account(item);
+  }
 
-    public mapper(item) {
-        return new Account(item);
-    }
+  public getPdf(account_id, lang: RecLang = REC_LANGS.ES) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'report_clients_providers'];
+    return this.get(
+      url,
+      {},
+      {
+        Accept: 'application/pdf',
+        'Content-Language': lang,
+        'Accept-Language': lang,
+      },
+      { responseType: 'blob' },
+    );
+  }
 
-    public getPdf(account_id, lang: RecLang = REC_LANGS.ES) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'report_clients_providers'];
-        return this.get(url, {}, {
-            'Accept': 'application/pdf',
-            'Content-Language': lang,
-            'Accept-Language': lang,
-        }, { responseType: 'blob' });
-    }
+  public getPdfAsHtml(account_id, lang: RecLang = REC_LANGS.ES) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'report_clients_providers'];
+    return this.get(
+      url,
+      {},
+      {
+        Accept: 'text/html',
+        'Content-Language': lang,
+        'Accept-Language': lang,
+      },
+      { responseType: 'text' },
+    );
+  }
 
-    public getPdfAsHtml(account_id, lang: RecLang = REC_LANGS.ES) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'report_clients_providers'];
-        return this.get(url, {}, {
-            'Accept': 'text/html',
-            'Content-Language': lang,
-            'Accept-Language': lang,
-        }, { responseType: 'text' });
-    }
+  public addConsumedProductToAccount(account_id, product_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'consuming_products'];
+    return this.post(url, { id: product_id }).pipe(this.itemMapper());
+  }
 
-    public addConsumedProductToAccount(account_id, product_id) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'consuming_products'];
-        return this.post(url, { id: product_id }).pipe(this.itemMapper());
-    }
+  public addProducedProductToAccount(account_id, product_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'producing_products'];
+    return this.post(url, { id: product_id }).pipe(this.itemMapper());
+  }
 
-    public addProducedProductToAccount(account_id, product_id) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'producing_products'];
-        return this.post(url, { id: product_id }).pipe(this.itemMapper());
-    }
+  public removeConsumedProductFromAccount(account_id, product_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'consuming_products', '/', product_id];
+    return this.delete(url).pipe(this.itemMapper());
+  }
 
-    public removeConsumedProductFromAccount(account_id, product_id) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'consuming_products', '/', product_id];
-        return this.delete(url).pipe(this.itemMapper());
-    }
+  public removeProducedProductFromAccount(account_id, product_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'producing_products', '/', product_id];
+    return this.delete(url).pipe(this.itemMapper());
+  }
 
-    public removeProducedProductFromAccount(account_id, product_id) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'producing_products', '/', product_id];
-        return this.delete(url).pipe(this.itemMapper());
-    }
+  public addActivity(account_id, activity_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'activities'];
+    return this.post(url, { id: activity_id }).pipe(this.itemMapper());
+  }
 
-    public addActivity(account_id, activity_id) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'activities'];
-        return this.post(url, { id: activity_id }).pipe(this.itemMapper());
-    }
+  public deleteActivity(account_id, activity_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'activities', '/', activity_id];
+    return this.delete(url).pipe(this.itemMapper());
+  }
 
-    public deleteActivity(account_id, activity_id) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'activities', '/', activity_id];
-        return this.delete(url).pipe(this.itemMapper());
-    }
+  // Campaigns
+  public addCampaing(account_id, campaign_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'campaigns'];
+    return this.post(url, { id: campaign_id }).pipe(this.itemMapper());
+  }
 
-    // Lemonway methods
-    public lwGetWallet(account_id) {
-        const url = [...this.getUrlBase(), '/', account_id, '/', 'integrations', '/', 'lemonway'];
-        return this.get(url, {});
-    }
+  public deleteCampaing(account_id, campaign_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'campaigns', '/', campaign_id];
+    return this.delete(url).pipe(this.itemMapper());
+  }
 
-    public lwSendFrom(amount, to, from) {
-        const url = [...this.getUrlBase(), '/', to, '/', 'integrations', '/', 'lemonway', '/', 'send-from'];
-        return this.post(url, { amount, from });
-    }
+  // Lemonway methods
+  public lwGetWallet(account_id) {
+    const url = [...this.getUrlBase(), '/', account_id, '/', 'integrations', '/', 'lemonway'];
+    return this.get(url, {});
+  }
 
-    public lwSendTo(amount, from, to) {
-        const url = [...this.getUrlBase(), '/', from, '/', 'integrations', '/', 'lemonway', '/', 'send-to'];
-        return this.post(url, { amount, to });
-    }
+  public lwSendFrom(amount, to, from) {
+    const url = [...this.getUrlBase(), '/', to, '/', 'integrations', '/', 'lemonway', '/', 'send-from'];
+    return this.post(url, { amount, from });
+  }
 
-    public createWithdrawal(account, amount, concept, otp, currency) {
-        const url = [...this.getUrlBase(), '/', account, '/', 'withdrawals'];
-        return this.post(url, { amount, concept, otp, currency });
-    }
+  public lwSendTo(amount, from, to) {
+    const url = [...this.getUrlBase(), '/', from, '/', 'integrations', '/', 'lemonway', '/', 'send-to'];
+    return this.post(url, { amount, to });
+  }
 
-    public createIBAN(account, data: Partial<Iban>) {
-        const url = [...this.getUrlBase(), '/', account, '/', 'ibans'];
-        return this.post(url, { ...data, account_id: account });
-    }
+  public createWithdrawal(account, amount, concept, otp, currency) {
+    const url = [...this.getUrlBase(), '/', account, '/', 'withdrawals'];
+    return this.post(url, { amount, concept, otp, currency });
+  }
 
-    public getIbans(account) {
-        const url = [...this.getUrlBase(), '/', account, '/', 'ibans'];
-        return this.get(url);
-    }
+  public createIBAN(account, data: Partial<Iban>) {
+    const url = [...this.getUrlBase(), '/', account, '/', 'ibans'];
+    return this.post(url, { ...data, account_id: account });
+  }
 
-    public getWithdrawals(account) {
-        const url = [...this.getUrlBase(), '/', account, '/', 'withdrawals'];
-        return this.get(url);
-    }
+  public getIbans(account) {
+    const url = [...this.getUrlBase(), '/', account, '/', 'ibans'];
+    return this.get(url);
+  }
 
-    public lwGateway(funct, data) {
-        const url = ['/', this.userRole, '/', this.version, '/gateway/lemonway/', funct];
-        return this.post(url, data);
-    }
+  public getWithdrawals(account) {
+    const url = [...this.getUrlBase(), '/', account, '/', 'withdrawals'];
+    return this.get(url);
+  }
 
-    public lwSendPayment(from, to, amount, concept) {
-        return this.lwGateway('SendPayment', {
-            amount: Number(amount).toFixed(2),
-            debitWallet: String(from),
-            creditWallet: String(to),
-            message: concept,
-        });
-    }
+  public lwGateway(funct, data) {
+    const url = ['/', this.userRole, '/', this.version, '/gateway/lemonway/', funct];
+    return this.post(url, data);
+  }
 
-    public lwMoneyOut(wallet, amount, message, otp) {
-        return this.lwGateway('MoneyOut', {
-            wallet,
-            amount,
-            message,
-            otp,
-        });
-    }
+  public lwSendPayment(from, to, amount, concept) {
+    return this.lwGateway('SendPayment', {
+      amount: Number(amount).toFixed(2),
+      debitWallet: String(from),
+      creditWallet: String(to),
+      message: concept,
+    });
+  }
 
-    public lwGetP2PList(wallets: string[]) {
-        return this.lwGateway('GetP2PTransactions', {
-            startDate: moment().subtract('1', 'year').unix(),
-            endDate: moment().unix(),
-            wallets,
-        });
-    }
+  public lwMoneyOut(wallet, amount, message, otp) {
+    return this.lwGateway('MoneyOut', {
+      wallet,
+      amount,
+      message,
+      otp,
+    });
+  }
 
-    public lwGetMoneyTxList(wallets: string[]) {
-        return this.lwGateway('GetMoneyTransactions', {
-            startDate: moment().subtract('1', 'year').unix(),
-            endDate: moment().unix(),
-            wallets,
-        });
-    }
+  public lwGetP2PList(wallets: string[]) {
+    return this.lwGateway('GetP2PTransactions', {
+      startDate: moment().subtract('1', 'year').unix(),
+      endDate: moment().unix(),
+      wallets,
+    });
+  }
+
+  public lwGetMoneyTxList(wallets: string[]) {
+    return this.lwGateway('GetMoneyTransactions', {
+      startDate: moment().subtract('1', 'year').unix(),
+      endDate: moment().unix(),
+      wallets,
+    });
+  }
 }
