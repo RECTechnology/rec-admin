58ddfaaad16072d2c1bf1b33d6e6b87db470de8b
Agut14 <86887600+Agut14@users.noreply.github.com>
2022-04-13 12:03:13 +0200

    fix(validation): added validation on b2b username with formControl. (#522)
    * fix(validation): added validation on b2b username with formControl.
    
    * added EmptyValidator abstract class and fixed translation.
    
    Co-authored-by: Carlos Agut <carlosagutvives@gmail.com>

--- src/components/selectors/account-picker/account-picker.component.ts
+++ src/components/selectors/account-picker/account-picker.component.ts
@@ -1,17 +1,38 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, SimpleChanges } from '@angular/core';
 import { AccountsCrud } from 'src/services/crud/accounts/accounts.crud';
 import { BaseSelectorComponent } from 'src/bases/base-selector';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { StreetTypeSelector } from '../street-selector/street-selector.component';
+import { Account } from '../../../shared/entities/account.ent';
 
 @Component({
   selector: 'account-picker', 
   templateUrl: './account-picker.component.html',
   styleUrls: ['./account-picker.component.scss'],
+  providers: [
+    {
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => AccountPickerComponent),
+        multi: true
+    }
+]
 })
 export class AccountPickerComponent extends BaseSelectorComponent {
 
   @Input() filters: any = null;
+  onChange!:(itemChanged: EventEmitter<any>) => void;
+    writeValue(account: Account): void {
+        if(account){
+            this.selectItem(account);
+        }
+    }
+    registerOnChange(fn: () => void): void {
+        this.onChange= fn;
+    }
+    registerOnTouched(fn: () => void): void {
+    }
 
   constructor( private accountsCrud: AccountsCrud){
     super()
--- src/pages/special-actions/b2b_page/b2b-modal/b2b-modal.ts
+++ src/pages/special-actions/b2b_page/b2b-modal/b2b-modal.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { AlertsService } from 'src/services/alerts/alerts.service';
 import { AccountsCrud } from 'src/services/crud/accounts/accounts.crud';
 import { Account } from 'src/shared/entities/account.ent';
 import { SendTransactionsDia } from '../../change_delegate/components/new_massive_transactions/send_transaction_modal/send_transactions_modal';
+import { EmptyValidators } from '../../../../components/validators/EmptyValidators';
 
 
 @Component({
@@ -19,6 +21,10 @@ export class AddB2BModal {
     public account: Account;
     public username = null;
     public filter = {"rezero_b2b_access": "not_granted"};
+    public formGroup = new FormGroup({
+        username: new FormControl("", [Validators.minLength(3), Validators.required, Validators.pattern(/^[A-Za-z0-9\_\.\-]+$/g), EmptyValidators.noWhiteSpace]),
+        account: new FormControl()
+    })
     
 
     constructor(
@@ -31,8 +37,10 @@ export class AddB2BModal {
     ngOnInit() { }
 
     ngOnChanges() { }
-
     public send() {
+        if(this.disabled || (!this.account.rezero_b2b_username && this.formGroup.invalid) || !this.formGroup.dirty){
+            return;
+        }
         this.dialogRef.close({
             accountId:this.account.id,
             username:this.username ?? this.account.rezero_b2b_username
