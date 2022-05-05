57220c8385af49f1d2b8543fc8967c7b79c874cf
Carlos Agut Vives <86887600+Agut14@users.noreply.github.com>
2022-05-05 11:13:21 +0200

    fixed/ date format when editing date manually. (#597)
    Co-authored-by: Carlos Agut <carlosagutvives@gmail.com>

--- src/components/selectors/date-picker/date-picker.ts
+++ src/components/selectors/date-picker/date-picker.ts
@@ -2,7 +2,36 @@
 import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
 import { DatePipe } from '@angular/common'
 import { FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { DateAdapter, NativeDateAdapter } from '@angular/material/core';
 
+export class CustomDateAdapter extends NativeDateAdapter {
+
+    parse(value: any): Date | null {
+
+    if ((typeof value === 'string') && (value.indexOf('/') > -1)) {
+       const str = value.split('/');
+
+      const year = Number(str[2]);
+      const month = Number(str[1]) - 1;
+      const date = Number(str[0]);
+
+      return new Date(year, month, date);
+    }
+    const timestamp = typeof value === 'number' ? value : Date.parse(value);
+    return isNaN(timestamp) ? null : new Date(timestamp);
+  }
+
+  format(date: Date, displayFormat: Object): string {
+    date = new Date(Date.UTC(
+      date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(),
+      date.getMinutes(), date.getSeconds(), date.getMilliseconds()));
+    displayFormat = Object.assign({}, displayFormat, { timeZone: 'utc' });
+
+    const dtf = new Intl.DateTimeFormat(this.locale, displayFormat);
+    return dtf.format(date).replace(/[\u200e\u200f]/g, '');
+  }
+
+}
 /** @title Basic datepicker */
 @Component({
     selector: 'date-picker',
@@ -15,7 +44,8 @@ import { FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
             provide: NG_VALUE_ACCESSOR,
             useExisting: forwardRef(() => DatePicker),
             multi: true
-        }
+        },
+        { provide: DateAdapter, useClass: CustomDateAdapter }
     ]
 })
 export class DatePicker {
