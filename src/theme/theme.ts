0478b80b310d9657861c879e4adfa4875bd69c26
Carlos Agut Vives <86887600+Agut14@users.noreply.github.com>
2022-11-10 15:37:17 +0100

    fix favicon, organization name and title (#738)
    Co-authored-by: Carlos Agut <carlosagutvives@gmail.com>

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,12 +1,12 @@
 import { environment } from '../environments/environment';
 export const Colors = {
-    REC: {
+    rec: {
         primary: '#0098db',
         txColors: ['#e05206', '#de8657'],
         regColors: ['#0098db', '#de8657'],
         neighColors: ['#0098db', '#9ed7f1', '#e05206', '#f0ab87', '#0555a5'],
     },
-    laROSA: {
+    larosa: {
         primary: '#E61872',
         txColors: ['#E61872', '#6BB900'],
         regColors: ['#E61872', '#6BB900'],
@@ -14,4 +14,4 @@ export const Colors = {
     }
 }
 
-export const theme = Colors.hasOwnProperty(environment.Brand.name) ?  Colors[environment.Brand.name] : Colors['REC'];
+export const theme = Colors.hasOwnProperty(environment.Brand.name) ?  Colors[environment.Brand.name] : Colors['rec'];
